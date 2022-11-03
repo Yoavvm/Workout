cf596d38a29ab9d5d9155ccfd59971e5c6c3394b
Toavvm <yoav.vm@gmail.com>
2022-11-03 14:19:30 +0200

    fixed nav bar to change upon login/logout

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,23 +1,33 @@
 
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom';
+import { logout } from '../../redux/features/user/UserSlice';
+import { useAppDispatch, useAppSelector } from '../../redux/Hooks';
 import '../styles.css';
 
 const Navbar = () => {
 
     const navigate = useNavigate();
-    const navigateOnClick = ({innerHTML}:any):void => {
-        navigate(innerHTML.toLowerCase())
+    const userState = useAppSelector(state => state.User)
+    const dispatch = useAppDispatch();
+    const navigateOnClick = ({ innerHTML }: any): void => {
+        innerHTML ? navigate(innerHTML.toLowerCase()) : navigate('')
+    }
+
+    const onLogoutClick = () => {
+        dispatch(logout())
+        navigateOnClick('')
     }
 
 
 
     return (
         <div className='navbar'>
-            <button onClick={(e) => navigateOnClick(e.target)}>Login</button>
-            <button onClick={(e) => navigateOnClick(e.target)}>Register</button>
+            {!userState.user && <button onClick={(e) => navigateOnClick(e.target)}>Login</button>}
+            {!userState.user && <button onClick={(e) => navigateOnClick(e.target)}>Register</button>}
+            {userState.user && <button onClick={onLogoutClick}>Logout</button>}
             <button onClick={(e) => navigateOnClick(e.target)}>Exercises</button>
-       
+
         </div>
     )
 }
--- client/src/redux/features/exercises/ExerciseSlice.ts
+++ client/src/redux/features/exercises/ExerciseSlice.ts
@@ -26,12 +26,7 @@ export const ExercisesReducer = createSlice(
         name: 'Exercise',
         initialState,
         reducers: {
-            login: (state, action: PayloadAction<[]>) => {
-                state.exercises = action.payload;
-            },
-            logout: (state) => {
-                state.exercises = null;
-            }
+            
         },
         extraReducers: (builder) => {
             builder.addCase(getAllExercises.fulfilled, (state, action:PayloadAction<any>) => {
@@ -44,7 +39,7 @@ export const ExercisesReducer = createSlice(
     }
 );
 
-export const { login, logout } = ExercisesReducer.actions;
+export const { } = ExercisesReducer.actions;
 
 export const selectExercises = (state: RootState) => state.Exercises;
 
